7e085fcc24d3e062f407340a496ed16ac3d86e82
gustavoevs <queondatavo@gmail.com>
2022-08-09 13:54:50 +0200

    Initial Commit

---
+++ consolidation5/consolidation5App.swift
@@ -0,0 +1,17 @@
+//
+//  consolidation5App.swift
+//  consolidation5
+//
+//  Created by Gustavo Verdugo on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct consolidation5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
